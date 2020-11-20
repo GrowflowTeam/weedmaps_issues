6eecb18db6606a5ab5949581fc632d24530ed742
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2020-11-19 21:32:06 -0700

    chore - add variable to change the number of total products that is expected to be returned

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-export const weedmapsAccessToken = process.env.WEEDMAPS_ACCESS_TOKEN || "replace_with_a_valid_token"
+export const weedmapsAccessToken =
+  process.env.WEEDMAPS_ACCESS_TOKEN || "replace_with_a_valid_token";
 
-export const orgID = process.env.WEEDMAPS_ORG_ID || "replace_with_an_org_id"
+export const orgID = process.env.WEEDMAPS_ORG_ID || "replace_with_an_org_id";
 
-export const catalogID = process.env.WEEDMAPS_CATALOG_ID || "replace_with_a_catalog_id"
+export const catalogID =
+  process.env.WEEDMAPS_CATALOG_ID || "replace_with_a_catalog_id";
--- issues/duplicatesWithPagedProductsRequest.spec.js
+++ issues/duplicatesWithPagedProductsRequest.spec.js
@@ -1,35 +1,43 @@
-import axios from 'axios';
-import { weedmapsAccessToken, orgID, catalogID } from '../config'
+import axios from "axios";
+import { weedmapsAccessToken, orgID, catalogID } from "../config";
 
 const defaultPageSize = 10;
 const totalNumberOfProductsExpected = 27;
 const testIterations = 3;
+const totalNumberOfPages = Math.ceil(
+  totalNumberOfProductsExpected / defaultPageSize
+);
 
 const createGetOptions = ({
   pageNumber = 1,
   pageSize = defaultPageSize,
 } = {}) => ({
-  method: 'GET',
-  url: 'https://api-g.weedmaps.com/catalog/v1/products/',
+  method: "GET",
+  url: "https://api-g.weedmaps.com/catalog/v1/products/",
   params: {
-    'scope_by[organization_id]': orgID,
-    'scope_by[catalog_id]': catalogID,
-    'filter[is_master]': 'false',
-    'filter[discontinued]': 'false',
-    'page[size]': pageSize,
-    'page[page]': pageNumber,
-    include: 'product_images',
+    "scope_by[organization_id]": orgID,
+    "scope_by[catalog_id]": catalogID,
+    "filter[is_master]": "false",
+    "filter[discontinued]": "false",
+    "page[size]": pageSize,
+    "page[page]": pageNumber,
+    include: "product_images",
   },
   headers: {
     authorization: `Bearer ${weedmapsAccessToken}`,
-    accept: 'application/vnd.api+json',
+    accept: "application/vnd.api+json",
   },
 });
 
 const fetchProducts = ({ pageNumber = 1, pageSize = defaultPageSize } = {}) =>
   axios(createGetOptions({ pageNumber, pageSize }));
 
-describe('duplicates with paged products request', () => {
+const createPageRequests = () =>
+  new Array(totalNumberOfPages)
+    .fill(null)
+    .map((_, index) => fetchProducts({ pageNumber: index + 1 }));
+
+describe("duplicates with paged products request", () => {
   new Array(testIterations).fill(null).forEach((_, index) => {
     describe(`Iteration: ${index + 1}`, () => {
       let uniqueIds;
@@ -37,11 +45,7 @@ describe('duplicates with paged products request', () => {
       let rawData;
 
       beforeEach(async () => {
-        rawData = await Promise.all([
-          fetchProducts(),
-          fetchProducts({ pageNumber: 2 }),
-          fetchProducts({ pageNumber: 3 }),
-        ]);
+        rawData = await Promise.all(createPageRequests());
       });
 
       beforeEach(() => {
@@ -53,11 +57,11 @@ describe('duplicates with paged products request', () => {
         uniqueIds = results.reduce((acc, { id }) => ({ ...acc, [id]: id }), {});
       });
 
-      it('the correct number of products are returned', () => {
+      it("the correct number of products are returned", () => {
         expect(results.length).toEqual(totalNumberOfProductsExpected);
       });
 
-      it('number of unique product ids should match the number of products retrieved', () => {
+      it("number of unique product ids should match the number of products retrieved", () => {
         expect(Object.keys(uniqueIds).length).toEqual(results.length);
       });
     });
